af607a2246ad7a4a65bf3f3960c97f57cab82635
Zaid Abdulameer <zabdulam33r@gmail.com>
2024-01-06 01:03:49 -0500

    Add today command

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
+import json
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
@@ -9,9 +9,11 @@ SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 tokens = {}
 
 def authorize(user: str) -> Credentials:
+    print(tokens)
     creds = None
     if tokens.get(user) != None:
-        creds = tokens.get(user)
+        creds = Credentials.from_authorized_user_info(tokens.get(user), SCOPES)
+        #creds = tokens.get(user)
 
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -22,8 +24,19 @@ def authorize(user: str) -> Credentials:
                 "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
-            tokens[user] = creds
+            tokens[user] = json.loads(creds.to_json())
+    writeFile()
     return creds
 
 if __name__ == "__main__":
     authorize("TEST")
+
+def writeFile():
+    with open("tokens.json", "w") as f:
+        f.write(json.dumps(tokens))
+
+with open("tokens.json", "r") as f:
+    content = f.read()
+    cache = json.loads(content)
+    for k, v in cache.items():
+        tokens[k] = v
