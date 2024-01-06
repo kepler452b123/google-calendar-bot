a78821186bf59149e263199178ece688c65c5e5a
Zaid Abdulameer <zabdulam33r@gmail.com>
2024-01-06 14:57:32 -0500

    Add embed for today command

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import datetime, time, date
 import os
 from dotenv import load_dotenv
 import discord
@@ -6,7 +6,7 @@ from discord.ext import commands
 from googleapiclient.discovery import build
 import auth
 
-service = {}
+services = {}
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -26,13 +26,25 @@ async def login(ctx):
     creds = auth.authorize(str(ctx.author.id))
     msg = f"Logged in!\nExpiry: {creds.expiry}\n"
 
-    global service
-    service = build("calendar", "v3", credentials=creds)
+    global services
+    services[ctx.author.id] = build("calendar", "v3", credentials=creds)
 
     await ctx.reply(msg)
 
+# Converts google "datetime" format to a simple 12 hour time format
+def get_time(date_str: str):
+    i = date_str.find("T")
+    time24 = date_str[i+1:i+6]
+    hour = int(time24[:2])
+    if hour >= 12:
+        hour %= 12
+        return f"{hour}{time24[2:]} PM"
+    else:
+        return f"{time24} AM"
+
 @bot.command()
 async def today(ctx):
+    service = services[ctx.author.id]
     start = datetime.combine(datetime.now(), time.min).isoformat() + "Z" # "Z" indicates UTC time
     end = datetime.combine(datetime.now(), time.max).isoformat() + "Z"
     events_result = (
@@ -40,11 +52,26 @@ async def today(ctx):
             calendarId="primary",
             timeMin=start,
             timeMax=end,
+            singleEvents=True,
+            orderBy="startTime"
         ).execute()
     )
-    msg = "" 
+    fields = []
     for e in events_result["items"]:
-        msg += "- " + e["summary"] + "\n"
-    await ctx.reply(msg)
+        time_start = get_time(e['start']['dateTime'])
+        time_end = get_time(e['end']['dateTime'])
+        field = {
+            "name": e["summary"],
+            "value": f"{time_start} - {time_end}"
+        }
+        fields.append(field)
+    embed = discord.Embed.from_dict({
+      "type": "rich",
+      "title": "Today",
+      "description": date.today(),
+      "color": 0x00FFFF,
+      "fields": fields
+    })
+    await ctx.reply(embed=embed)
 
 bot.run(TOKEN)
